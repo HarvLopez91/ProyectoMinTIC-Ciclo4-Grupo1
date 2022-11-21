89e0b9a4bf35e1330cf7351b4bae7f9b181b32d4
Jhan Meza <ing.jhanmeza@gmail.com>
2022-11-20 22:52:28 -0500

    correcion de btn configurar perfil, actualizacion de archivo sprint 2

--- MiIngresoSeguroApp/app/src/main/java/co/edu/unab/misiontic2022/miingresoseguroapp/MainActivity.java
+++ MiIngresoSeguroApp/app/src/main/java/co/edu/unab/misiontic2022/miingresoseguroapp/MainActivity.java
@@ -4,18 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "CV_Login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
     }
-    public void configUsuario(View view){
-        startActivity(new Intent(this,ConfPerfilActivity.class));
-    }
+
+
     public void inicio(View view){
         startActivity(new Intent(this,PerfilActivity.class));
     }
